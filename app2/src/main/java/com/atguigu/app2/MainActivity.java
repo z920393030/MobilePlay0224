f0ef683e42fb6600b0a2b800e01167de8ba35a74
z920393030 <9203930303@qq.com>
2017-05-22 19:25:12 +0800

    完成主页面布局

--- app2/src/main/java/com/atguigu/app2/MainActivity.java
+++ app2/src/main/java/com/atguigu/app2/MainActivity.java
@@ -2,12 +2,15 @@ package com.atguigu.app2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RadioGroup rg_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rg_main = (RadioGroup)findViewById(R.id.rg_main);
+
     }
 }
