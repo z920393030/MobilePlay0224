d86bc92b9b8e92d73fd0c024cad0a9e147ff69ff
z920393030 <9203930303@qq.com>
2017-05-22 19:38:06 +0800

    4个页面显示的Fragment

---
+++ app2/src/main/java/com/atguigu/app2/fragment/BaseFragment.java
@@ -0,0 +1,43 @@
+package com.atguigu.app2.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by My on 2017/5/22.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    public Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+    }
+
+    public abstract View initView() ;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.e("TAG","BaseFragment-onActivityCreated");
+        initData();
+    }
+
+    public void initData() {
+
+    }
+}
---
+++ app2/src/main/java/com/atguigu/app2/pager/NetVideoPager.java
@@ -0,0 +1,32 @@
+package com.atguigu.app2.pager;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.app2.fragment.BaseFragment;
+
+/**
+ * Created by My on 2017/5/22.
+ */
+
+public class NetVideoPager extends BaseFragment {
+    private TextView textView;
+
+
+    @Override
+    public View initView() {
+        textView = new TextView(context);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("网络视频的内容");
+    }
+}
