a3fc6cb5cb590003dd01d67b641fcf9da17e5d7b
z920393030 <9203930303@qq.com>
2017-05-22 19:47:29 +0800

    得到本地视频信息

--- app2/src/main/java/com/atguigu/app2/pager/LocalVideoPager.java
+++ app2/src/main/java/com/atguigu/app2/pager/LocalVideoPager.java
@@ -1,32 +1,66 @@
 package com.atguigu.app2.pager;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.atguigu.app2.R;
+import com.atguigu.app2.domain.MediaItem;
 import com.atguigu.app2.fragment.BaseFragment;
+
+import java.util.ArrayList;
 
 /**
  * Created by My on 2017/5/22.
  */
 
 public class LocalVideoPager extends BaseFragment {
-    private TextView textView;
+    private ListView lv;
+    private TextView tv_nodata;
+    private ArrayList<MediaItem> mediaItems;
 
 
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View  view = View.inflate(context, R.layout.fragment_local_video_pager,null);
+        lv = (ListView) view.findViewById(R.id.lv);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+        return view;
     }
 
     @Override
     public void initData() {
-        super.initData();
-        textView.setText("本地视频的内容");
+        new Thread(){
+            public void run(){
+                mediaItems = new ArrayList<MediaItem>();
+                ContentResolver resolver = context.getContentResolver();
+                Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                String[] objs = {
+                        MediaStore.Video.Media.DISPLAY_NAME,//视频在sdcard上的名称
+                        MediaStore.Video.Media.DURATION,//视频时长
+                        MediaStore.Video.Media.SIZE,//视频文件的大小
+                        MediaStore.Video.Media.DATA//视频播放地址
+                };
+                Cursor cursor = resolver.query(uri, objs, null, null, null);
+                if(cursor != null ){
+                    while (cursor.moveToNext()){
+                        String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                        long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                        long size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                        String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                        Log.e("TAG","name=="+name+",duration=="+duration+",data==="+data);
+
+                        mediaItems.add(new MediaItem(name,duration,size,data));
+                    }
+
+                    cursor.close();
+                }
+            }
+        }.start();
     }
 }
