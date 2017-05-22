69beb3e8ff45909c937ddb50b254d7a858918bdf
z920393030 <9203930303@qq.com>
2017-05-22 19:42:04 +0800

    解决横竖屏切换内容重叠的问题

--- app2/src/main/java/com/atguigu/app2/MainActivity.java
+++ app2/src/main/java/com/atguigu/app2/MainActivity.java
@@ -1,5 +1,9 @@
 package com.atguigu.app2;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -83,5 +87,25 @@ public class MainActivity extends AppCompatActivity {
             tempFragment = currentFragment;
 
         }
+    }
+
+    /**
+     * 解决安卓6.0以上版本不能读取外部存储权限的问题
+     * @param activity
+     * @return
+     */
+    public static boolean isGrantExternalRW(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            activity.requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+            return false;
+        }
+
+        return true;
     }
 }
